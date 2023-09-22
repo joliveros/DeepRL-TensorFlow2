9f41bb7f15e6d3c764a4bd737d17e3c6e6a88fd8
Jose Oliveros <jose.oliveros.1983@gmail.com>
2023-09-21 23:29:04 -0600

    refactor, random predictions for first episode
    Signed-off-by: Jose Oliveros <jose.oliveros.1983@gmail.com>

--- A3C/worker_agent.py
+++ A3C/worker_agent.py
@@ -86,7 +86,6 @@ class WorkerAgent(Thread):
 
     def train(self, *args, **kwargs):
         global CUR_EPISODE
-        capital = None
 
         while self.max_episodes >= CUR_EPISODE:
             episode_reward, done = 0, False
@@ -94,69 +93,8 @@ class WorkerAgent(Thread):
             state = self.env.reset()
 
             while not done:
-                if state is None:
-                    state = np.zeros(self.state_dim)
-
-                probs = self.actor.model.predict(np.asarray([state]))
-
-                alog.info(probs)
-
-                action = np.random.choice(self.action_dim, p=probs[0])
-
-                # action = np.argmax(probs[0])
-
-                next_state, reward, done, _ = self.env.step(action)
-
-                self.env_state = _
-
-                state = np.asarray([state])
-                action = np.reshape(action, [1, 1])
-                next_state = np.asarray([next_state])
-                reward = np.reshape(reward, [1, 1])
-
-                self.cache.append([state, action, reward])
-
-                if done:
-                    wandb.log({'train_capital': self.env_state['capital']})
-
-                if (self.n_steps % self.update_interval == 0 or done) and \
-                    len(self.cache)> self.batch_size:
-
-                    states = []
-                    actions = []
-                    rewards = []
-                    cache_len = len(self.cache)
-                    for i in range(0, self.batch_size):
-                        ix = np.random.randint(0, cache_len)
-                        state, action , reward = self.cache[ix]
-                        states.append(state[0])
-                        actions.append(action[0])
-                        rewards.append(reward[0])
-
-                    states = np.asarray(states)
-                    actions = np.asarray(actions)
-                    rewards = np.asarray(rewards)
-
-                    next_v_value = self.critic.model.predict(next_state)
-                    td_targets = self.n_step_td_target(
-                        rewards, next_v_value, done)
-                    advantages = td_targets - self.critic.model.predict(states)
-
-                    with self.lock:
-                        actor_loss = self.global_actor.train(
-                            states, actions, advantages)
-                        critic_loss = self.global_critic.train(
-                            states, td_targets)
-
-                        self.actor.model.set_weights(
-                            self.global_actor.model.get_weights())
-                        self.critic.model.set_weights(
-                            self.global_critic.model.get_weights())
-
-                    wandb.log(dict(actor_loss=actor_loss.numpy(),
-                                   critic_loss=critic_loss.numpy()))
-
-                    self.n_steps += 1
+                done, next_state, reward = \
+                    self.step(CUR_EPISODE, done, state)
 
                 episode_reward += reward[0][0]
                 state = next_state[0]
@@ -170,6 +108,66 @@ class WorkerAgent(Thread):
 
         wandb.run.summary["final capital"] = self.env_state['capital']
         wandb.run.summary["state"] = "completed"
+
+    def step(self, CUR_EPISODE, done, state):
+        if state is None:
+            state = np.zeros(self.state_dim)
+        if CUR_EPISODE > 1:
+            probs = self.actor.model.predict(np.asarray([state]))
+            alog.info(probs)
+            action = np.random.choice(self.action_dim, p=probs[0])
+        else:
+            action = np.random.choice(self.action_dim, p=[0.2, 0.8])
+        # action = np.argmax(probs[0])
+        next_state, reward, done, _ = self.env.step(action)
+        self.env_state = _
+        state = np.asarray([state])
+        action = np.reshape(action, [1, 1])
+        next_state = np.asarray([next_state])
+        reward = np.reshape(reward, [1, 1])
+        self.cache.append([state, action, reward])
+        if done:
+            wandb.log({'train_capital': self.env_state['capital']})
+        if (self.n_steps % self.update_interval == 0 or done) and \
+                len(self.cache) > self.batch_size:
+
+            states = []
+            actions = []
+            rewards = []
+            cache_len = len(self.cache)
+            for i in range(0, self.batch_size):
+                ix = np.random.randint(0, cache_len)
+                state, action, reward = self.cache[ix]
+                states.append(state[0])
+                actions.append(action[0])
+                rewards.append(reward[0])
+
+            states = np.asarray(states)
+            actions = np.asarray(actions)
+            rewards = np.asarray(rewards)
+
+            next_v_value = self.critic.model.predict(next_state)
+            td_targets = self.n_step_td_target(
+                rewards, next_v_value, done)
+            advantages = td_targets - self.critic.model.predict(states)
+
+            with self.lock:
+                actor_loss = self.global_actor.train(
+                    states, actions, advantages)
+                critic_loss = self.global_critic.train(
+                    states, td_targets)
+
+                self.actor.model.set_weights(
+                    self.global_actor.model.get_weights())
+                self.critic.model.set_weights(
+                    self.global_critic.model.get_weights())
+
+            wandb.log(dict(actor_loss=actor_loss.numpy(),
+                           critic_loss=critic_loss.numpy()))
+
+            self.n_steps += 1
+
+        return done, next_state, reward
 
     def run(self):
         return self.train()
